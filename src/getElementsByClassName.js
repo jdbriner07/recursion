e45b7908d579e013c0fa00d7e001b493fd95f274
Jeffrey Briner <jdbriner07@gmail.com>
2017-01-30 03:36:52 -0800

    early on, this it will break

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	bodyObject = document.body;
+	var acc = [];
+	if (bodyObject.classList.contains(className)){
+		acc.push(bodyObject);
+	}
+	acc.push(checker(bodyObject, className));
+	console.log(acc);
 };
+
+var checker = function(bodyObject, className) {
+	children = bodyObject.childNodes;
+	if (children === undefined){
+		return 1;
+	}
+	if(!children[0].classList){
+		bodyObject.removeChild(bodyObject.firstChild);
+	}
+	if(children[0].classList){
+		
+		return 1;
+	}
+}
